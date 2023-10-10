67770bf1fe24dc4e0041df5c597807018ad68bec
Associate <Associate@roifmrwindt>
2023-10-10 17:01:39 +0530

    Completed the design for catalogue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { DataViewModule} from 'primeng/dataview';
+import { RatingModule } from 'primeng/rating';
+import { FormsModule } from '@angular/forms';
+import { MenubarModule } from 'primeng/menubar';
+
 import { AppComponent } from './app.component';
+import { CatalogueComponent } from './catalogue/catalogue.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatalogueComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DataViewModule,
+    RatingModule,
+    FormsModule,
+    MenubarModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
