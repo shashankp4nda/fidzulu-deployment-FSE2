febe864a60a6637d14c489be0dfb2f838437f248
Associate <Associate@roifmrwindt>
2023-10-11 10:59:05 +0530

    fixed test cases

--- src/app/services/food.service.ts
+++ src/app/services/food.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Food } from '../models/food.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FoodService {
 
-  constructor() { }
+  foods: Food[]= [];
+  private foodUrl="";
+
+  getFoods(): Observable<Food[]>{
+    return this.http.get<Food[]>(this.foodUrl);
+  }
+
+  constructor(private http: HttpClient) { }
 }
--- src/app/services/toy.service.spec.ts
+++ src/app/services/toy.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 import { ToyService } from './toy.service';
 
 describe('ToyService', () => {
   let service: ToyService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(ToyService);
   });
 
