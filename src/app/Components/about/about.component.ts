9b5b5ffcc1223da0d730e3acf2e156a98d4a076e
Associate <Associate@roifmrwindt>
2023-10-11 11:39:39 +0530

    resized the image

--- src/app/Components/about/about.component.ts
+++ src/app/Components/about/about.component.ts
@@ -9,11 +9,13 @@ export class AboutComponent {
   
   items: any = [
     ["Front-End team", "Angular", "https://content.mycareersfuture.gov.sg/wp-content/uploads/2023/05/People-Person-Quiz-1.jpg", "As a memeber of the front end team, I can comfirm that the front end team is the best"],
-    ["Middle-Tier team", "NodeJS", "https://content.mycareersfuture.gov.sg/wp-content/uploads/2023/05/People-Person-Quiz-1.jpg", "our middle tier team is also good"],
-    ["Back-End team", "SpringBoot", "https://content.mycareersfuture.gov.sg/wp-content/uploads/2023/05/People-Person-Quiz-1.jpg", "and guess what? our backend team is pretty good too"]
+    ["Middle-Tier team", "NodeJS", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjkgXmGZFnXYKPoyM1ZLH-pqppZkZe52MLd-wwbcf6MkhqOLKEn0QR7w72mZPucvAmEwA&usqp=CAU", "our middle tier team is amazing"],
+    ["Back-End team", "SpringBoot", "https://www.webdesignlane.com/assets/images/av/03.jpg", "and guess what? our backend team is pretty good too"]
   ];
-}
 
+
+
+}
 
 
 
