683425ca0ad062c44b27bc09d4680afe5015fed1
Associate <Associate@roifmrwindt>
2023-10-12 11:54:33 +0530

    fixed bugs of filter

--- src/app/services/toy.service.ts
+++ src/app/services/toy.service.ts
@@ -2,17 +2,20 @@ import { Injectable } from '@angular/core';
 import { Toy } from '../models/toy.model';
 import { Observable, catchError, throwError } from 'rxjs';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { DropDownService } from './drop-down.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ToyService {
   toys: Toy[]= [];
-  public toyUrl="";
+  public toyUrl="http://54.90.86.99:3021/toys/all/IN";
   public url="";
 
   getToys(): Observable<Toy[]>{
-    return this.http.get<Toy[]>(this.url).pipe(catchError(this.handleError));
+    this.url =  this.toyUrl + this.dropDownService.getSelectedValue();
+    console.log(this.url);
+    return this.http.get<Toy[]>(this.toyUrl).pipe(catchError(this.handleError));
   }
 
   handleError(response: HttpErrorResponse) { 
@@ -27,11 +30,11 @@ export class ToyService {
     return throwError( () => 'Unable to contact service; please try again later.'); 
   }
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private dropDownService: DropDownService) { }
 
-  processSelectedCountry(selectedCountry: string) {
-    this.url =  this.toyUrl + selectedCountry;
-    console.log(`Selected value is: ${this.url}`);
-  }
+  // processSelectedCountry(selectedCountry: string) {
+  //   this.url =  this.toyUrl + selectedCountry;
+  //   console.log(`Selected value is: ${this.url}`);
+  // }
  
 }
