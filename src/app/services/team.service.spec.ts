dbfad32bb0577ce81a1424cb36f40f875397c06b
Associate <Associate@roifmrwindt>
2023-10-12 12:18:30 +0530

    fixed merge conflicts in team service

--- src/app/services/team.service.spec.ts
+++ src/app/services/team.service.spec.ts
@@ -2,18 +2,13 @@ import { TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { TeamService } from './team.service';
-<<<<<<< HEAD
 import { inject } from '@angular/core/testing';
 import { Team } from '../models/team.model';
 import { HttpErrorResponse } from '@angular/common/http';
-=======
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
->>>>>>> 3509b21c7031cfaa6d7a18ca9d22c1345478c36f
 
 describe('TeamService', () => {
   let service: TeamService;
   let httpTestingController: HttpTestingController;
-<<<<<<< HEAD
 
   let testBikeTeam: Team[] = [
   {
@@ -38,30 +33,12 @@ describe('TeamService', () => {
       ]
     });
     httpTestingController = TestBed.inject(HttpTestingController);
-=======
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({imports: [
-      HttpClientTestingModule
-    ]});
->>>>>>> 3509b21c7031cfaa6d7a18ca9d22c1345478c36f
     service = TestBed.inject(TeamService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
-
-  it('should return the values for the bike team', inject([TeamService], fakeAsync((service: TeamService) => {
-    let teams: Team[]=[];
-    service.getBikeTeam().subscribe(data => {teams = data});
-    const req = httpTestingController.expectOne(service.bikeTeamUrl);
-    expect(req.request.method).toEqual('GET');
-      req.flush(testBikeTeam);
-      tick();
-      expect(teams[0].team).toBe('Front-End');
-      expect(teams[2].memberNames[0]).toContain('Jyothsna');
-  })))
 
 
   it('should handle a 404 error for Bike Team', inject([TeamService], fakeAsync((service: TeamService) => { 
